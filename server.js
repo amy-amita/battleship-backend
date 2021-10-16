a72ee50245de6894894a226c8f8a25bf8ebb3c1a
Pump P <patsanop@gmail.com>
2021-10-17 02:09:40 +0700

    userSchema

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ console.log("Server Started!");
 io.on("connection", (socket) => {
   console.log(socket.id);
 
-  socket.on("messageToServer", (message, room) => {
-      socket.to(room).emit("messageToClient", message);
+  socket.on("messageToServer", (message, roomId) => {
+      socket.to(roomId).emit("messageToClient", message);
   });
   // socket.on("join-room", (room, callback) => {
   //   socket.join(room);
