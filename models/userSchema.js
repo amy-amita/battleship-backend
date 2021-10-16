03302bbda1a39450ed7d092c4c81e07a422b2de3
amy-amita <k.amitamy@gmail.com>
2021-10-17 02:13:14 +0700

    user schema fix pump's stupid mistake

--- models/userSchema.js
+++ models/userSchema.js
@@ -3,14 +3,14 @@ const mongoose = require("mongoose");
 const userSchema = mongoose.Schema({
   username: String,
   socketId: String,
-  roomId: number,
-  avatarNo: number,
-  });
+  roomId: Number,
+  avatarNo: Number,
+});
 
-userSchema.statics.validate = async function(username){
+userSchema.statics.validate = async function (username) {
   const foundUser = await this.findOne({ username });
-  if(!foundUser) return false;
+  if (!foundUser) return false;
   return foundUser;
-}
+};
 
 module.exports = mongoose.model("User", userSchema);
