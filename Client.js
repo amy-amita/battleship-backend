d0a95d2cae2fa9aee1070b370d40e60977360c24
Pump P <patsanop@gmail.com>
2021-10-17 01:49:53 +0700

    database skeleton

--- Client.js
+++ Client.js
@@ -1,6 +1,23 @@
 const io = require("socket.io-client");
+const readline = require("readline");
+const prompt = require("prompt-sync")();
 
 const socket = io("http://localhost:3031");
 socket.on("connect", () => {
   console.log(socket.id);
 });
+
+let room = socket.id;
+
+const message = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+message.question("message: ");
+
+
+socket.emit('messageToServer', message, room);
+
+socket.emit("join-room", room => { 
+
+})
--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const io = require("socket.io")(process.env.PORT || 3031);
+const express = require("express");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const User = require("./models/userSchema");
+const room = require("./models/roomSchema");
 
 console.log("Server Started!");
 
 io.on("connection", (socket) => {
   console.log(socket.id);
+
   socket.on("messageToServer", (message, room) => {
       socket.to(room).emit("messageToClient", message);
   });
-  socket.on("join-room", (room, callback) => {
-    socket.join(room);
-    callback(`Joined Room ${room}`);
-  });
+  // socket.on("join-room", (room, callback) => {
+  //   socket.join(room);
+  //   callback(`Joined Room ${room}`);
+  // });
   socket.on("disconnect", () => {
     console.log("Disconnected!");
   });
