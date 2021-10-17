68392358eaccd0d046f6e852980d2bc010fe2c70
amy-amita <k.amitamy@gmail.com>
2021-10-17 22:40:31 +0700

    just commit to pull

--- server.js
+++ server.js
@@ -4,14 +4,28 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const User = require("./models/userSchema");
 const room = require("./models/roomSchema");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
 
 console.log("Server Started!");
+
+mongoose.connect(
+  "mongodb://localhost:27017/realtimeProject",
+  { useNewUrlParser: true },
+  function (err) {
+    if (err) {
+      throw err;
+    }
+    console.log("Database connected");
+  }
+);
 
 io.on("connection", (socket) => {
   console.log(socket.id);
 
   socket.on("messageToServer", (message, roomId) => {
-      socket.to(roomId).emit("messageToClient", message);
+    socket.to(roomId).emit("messageToClient", message);
   });
   // socket.on("join-room", (room, callback) => {
   //   socket.join(room);
