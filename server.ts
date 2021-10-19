aad6750472e06af5ff10eccc224e16cf95cd623f
amy-amita <k.amitamy@gmail.com>
2021-10-19 23:41:08 +0700

    edit check username and add check room full

--- server.ts
+++ server.ts
@@ -5,9 +5,11 @@ import User from './models/userSchema'
 import Room from './models/roomSchema'
 
 //------------------------------------------- Connect to Database -------------------------------------------------//
+
 mongoose.connect(
     'mongodb+srv://testuser:battleship@cluster0.w9j5l.mongodb.net/battleship?retryWrites=true&w=majority'
 )
+
 //-----------------------------------------------------------------------------------------------------------------//
 
 const io = new Server(3031)
@@ -16,19 +18,22 @@ io.on('connection', (socket) => {
     console.log(socket.id)
 
     // open page
-    socket.on('userData', async (username, avatarName) => {
+    socket.on('userData', async (username: string, avatarName: string) => {
+        let checkUsername = true
         console.log(username)
-        // const user = new User({ username, socketId: socket.id, avatarName });
-        // const valid = await User.findOne({ username });
-        // if (!valid) {
-        //   await user.save();
-        // } else {
-        //   socket.emit("messageToClient", 1); //USERNAME IS ALREADY TAKEN
-        // }
+        const user = new User({ username, socketId: socket.id, avatarName })
+        const valid = await User.findOne({ username })
+        if (!valid) {
+            await user.save()
+            socket.emit('checkUsername', checkUsername, username)
+        } else {
+            checkUsername = false
+            socket.emit('checkUsername', checkUsername, username) //USERNAME IS ALREADY TAKEN
+        }
     })
 
     // create game
-    socket.on('createGame', async (username, cb) => {
+    socket.on('createGame', async (username: string, cb: string) => {
         const room = new Room({
             roomId: uuidv4(),
             playerOneName: username,
@@ -44,17 +49,21 @@ io.on('connection', (socket) => {
             time: 10,
         })
         await room.save()
-        cb(`Room ID : ${room.roomId}`)
+        // cb(`Room ID : ${room.roomId}`)
     })
 
-    // join game
-    socket.on('joinGame', async (username: string, roomId: string, cb) => {
-        const filter = { roomId }
-        const update = { playerTwoName: username }
-        const room = await Room.findOneAndUpdate(filter, update, {
-            new: true,
-        })
-        cb(`Joined Room ${room.roomId}`)
+    //join game
+    socket.on('joinGame', async (username: string, roomId: string, cb: any) => {
+        if (Room.findOne({ roomId }).playerTwoName === '') {
+            const filter = { roomId }
+            const update = { playerTwoName: username }
+            await Room.findOneAndUpdate(filter, update, {
+                new: true,
+            })
+            cb(roomId)
+        } else {
+            cb(`This room is full!`)
+        }
     })
 
     socket.on('messageToServer', (message: string, roomId: string) => {
