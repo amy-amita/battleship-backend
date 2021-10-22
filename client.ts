00d8a811d0a0f6788bc42952b431a0cb2460653f
Pump P <patsanop@gmail.com>
2021-10-22 23:17:50 +0700

    client add

--- client.ts
+++ client.ts
@@ -1,7 +1,4 @@
 import io from 'socket.io-client'
-import readline from 'readline'
-import { format } from 'path'
-import { SSL_OP_NO_TICKET } from 'constants'
 
 // const jsdom = require("jsdom");
 // const { JSDOM } = jsdom;
@@ -22,53 +19,81 @@ import { SSL_OP_NO_TICKET } from 'constants'
 //   messageInput.value = "";
 // })
 
-const socket = io('http://localhost:3031')
+
+
+
+const socket = io('https://4f97-2001-fb1-9d-1698-bc04-61b-894a-5ce0.ngrok.io/')
 socket.on('connect', () => {
     console.log(socket.id)
-})
+});
 
-//create game
-// socket.emit('createGame', 'test001', (cb: any) => {
-//     console.log(cb)
-// });
+while(true){
 
-//join game
-// socket.emit('joinGame', '2a4de412-2f51-4fcf-88e7-09d83abe3a12', 'james', (cb: any) => {
-//     console.log(cb)
-// });
 
-// //ready w/ username
-// socket.emit('ready', 'test001', '30,20,10,00,73,63,53,43,04,14,24,34,02,12,22,32', (cb: any) => {
-//     console.log(cb)
-// });
+        if(choice === 1){
+            //create game
+            socket.emit('createGame', 'test001', (cb: any) => {
+                console.log(cb)
+            });
+        }
 
-// //ready w/ roomId
-// socket.emit('ready', '2a4de412-2f51-4fcf-88e7-09d83abe3a12', 'james', '30,20,10,00,73,63,53,43,04,14,24,34,02,12,22,32', (cb: any) => {
-//     console.log(cb)
-// });
+        if(choice === 2) {
+            //join game
+            socket.emit('joinGame', 'a4f1e5bf-8dbf-474e-b912-ebfb15a07978', 'james', (cb: any) => {
+                console.log(cb)
+                });
+        }
 
-// //attack w/ username
-// socket.emit('attack', 'test001', '33', (cb: any) => {
-//     console.log(cb)
-// });
+        if(choice === 3){
+            //ready w/ roomId
+            socket.emit('ready', 'a4f1e5bf-8dbf-474e-b912-ebfb15a07978', 'test001', '30,20,10,00,73,63,53,43,04,14,24,34,02,12,22,32', (cb: any) => {
+                console.log(cb)
+            });
+        }
 
-//attack w/ roomId
-// socket.emit(
-//     'attack',
-//     '2a4de412-2f51-4fcf-88e7-09d83abe3a12',
-//     'james',
-//     '11',
-//     (cb: any) => {
-//         console.log(cb)
-//     }
-// )
+        if(choice === 4){
+            //attack w/ roomId
+            socket.emit('attack', '2a4de412-2f51-4fcf-88e7-09d83abe3a12', 'james', '11',
+                (cb: any) => {
+                    console.log(cb)
+            })
+        }
 
-socket.emit('findRoom')
+        if(choice === 5){
+            // socket.emit('findRoom')
+            socket.on('findRoom', (rooms) => {
+                console.log(rooms)
+            });
+        }
 
-socket.on('findRoom', (rooms) => {
-    console.log(rooms)
-})
+        if(choice === 6){
+            socket.on('onlineNum', (count) => {
+                console.log(`Player Online: ${count}`)
+            });
+        }
+        rl.close();
+    });
+}
 
-socket.on('onlineNum', (count) => {
-    console.log(`Player Online: ${count}`)
-})
+class SomeClass
+{
+    constructor(readline) {
+        this.readline = readline;
+    }
+
+    askPath() {
+        this.readline.question('Question: ', (answer) => {
+            console.log(answer);
+            this.readline.close();
+            this.askPath();
+        });
+    }
+}
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let someClass = new SomeClass(rl);
+someClass.askPath();
