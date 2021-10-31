568fb79c9107503bcc282e2be5a8e0101584761a
amy-amita <k.amitamy@gmail.com>
2021-11-01 00:24:57 +0700

    add event to joinGame

--- client.ts
+++ client.ts
@@ -29,10 +29,8 @@ socket.on('connect', () => {
 //     console.log(cb)
 // });
 
-// //join game
-// socket.emit('joinGame', 'c069323e-09dc-4394-b1f3-37969a669f37', 'james', (cb: any) => {
-//     console.log(cb)
-//     });
+//join game
+socket.emit('joinGame', 'c069323e-09dc-4394-b1f3-37969a669f37', 'boom')
 
 // // findRoom
 // socket.emit('findRoom');
@@ -70,8 +68,8 @@ socket.on('connect', () => {
 // socket.on('chat', (username:string, message:string)=> {
 //     console.log(username, message);
 // })
-socket.emit('findRoom')
-socket.on('findRoom', (rooms) => {
-    console.log(rooms)
-    console.log(typeof rooms)
-})
+// socket.emit('findRoom')
+// socket.on('findRoom', (rooms) => {
+//     console.log(rooms)
+//     console.log(typeof rooms)
+// })
